e0e8ab7bdf0f2a3b78db78b45c3a4df487a344bc
Roshan <rkplaxonic@gmail.com>
2021-03-19 00:54:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import  android.view.MenuItem;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+   CardView cardView;
+   BottomAppBar bottomAppBar;
+   RelativeLayout searchRel;
+   FloatingActionButton fab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cardView=findViewById(R.id.card);
+        cardView.setBackgroundResource(R.drawable.normal_edit_text_bg);
+        searchRel=findViewById(R.id.search_rel);
+        fab=findViewById(R.id.fab);
+        searchRel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Activity_Search.class);
+                startActivity(intent);
+            }
+        });
+        fab.setColorFilter(Color.WHITE);
+//        inflateBottomAppBar();
+    }
+
+//    private boolean inflateBottomAppBar() {
+//        bottomAppBar = findViewById(R.id.bottombar);
+//        Menu bottomMenu = bottomAppBar.getMenu();
+//        getMenuInflater().inflate(R.menu.bottom_app_menu, bottomMenu);
+//        for (int i = 0; i < bottomMenu.size(); i++) {
+//            bottomMenu.getItem(i).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+//                @Override
+//                public boolean onMenuItemClick(MenuItem menuItem) {
+//                    return onOptionsItemSelected(menuItem);
+//                }
+//            });
+//        }
+//        return super.onCreateOptionsMenu(bottomMenu);
+//    }
+}
